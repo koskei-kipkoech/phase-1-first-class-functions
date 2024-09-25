255a6ec8ebce98e28f10492aaa9f5e8c8f3ce3dc
Patrick Kipkoech <patrickwayy@gmail.com>
2024-09-25 08:28:19 +0300

    Updates

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(callback){
+    callback();
+}
+
+function returnsANamedFunction(){
+    return function namedFunction(){
+        console.log("I am a named function")
+    }
+}
+function returnsAnAnonymousFunction(){
+    return function(){
+        console.log("I am a return function")
+    }
+}
